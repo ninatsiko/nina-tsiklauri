6de9d9ddc2e7c90cc76ea5a0d08e414110a73629
nina tsiklauri <101926937+ninatsiko@users.noreply.github.com>
2022-06-14 16:49:40 +0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,65 +1,96 @@
-//task 1
-let x = 28;
-let y = 21;
-let k = ('ანა ლევანიზე 7 წლით უფროსია');
-let z=x-y;
-console.log(z);
-console.log(k);
+// task 1 
+
+let sampleObject = {
+    isItarable : true,
+    sampleArray : [12,63,21,34,98,57]
+}
+
+function myFunction (object){
+    if(object.isItarable == 1){
+        for (i = 0; i < object.sampleArray.length; i++)
+        console.log(object.sampleArray[i])
+    }
+    else {
+        console.log('provided array is not itarable')
+    }
+
+}
+myFunction(sampleObject); 
 
 //task 2
 
-let StudentsNames = ['John', 'Sam', 'Ann', 'Will', 'Andrew', 'joseph', 'Lorelai', 'Amelie', 'Den'];
-let MiddleNumber = ((StudentsNames.length - 1) / 2);
-let MiddleName = (StudentsNames[MiddleNumber]);
-console.log(MiddleName);
-
-//task 3  
-//part 1
-
-var student = {
-    firstName : 'Nina',
-    lastName : 'tsiklauri',
-    age : 21,
-    subjects : ['math', 'javascript', 'history', 'english', 'economy'],
-    roommate : {
-        fullName : 'ani devdariani',
-        age : 21,
+function checkPythagoras (x,y,z){
+    switch(true){
+        case (x**2 + y**2 == z**2):
+            return true;
+        case (x**2 + z**2 == y**2):
+            return true;
+        case (y**2 + z**2 == x**2):
+            return true;
+        default:
+            return false;
     }
 }
-//part 2 
-for (let element of student.subjects){
-    console.log (element)
-}
+let result = checkPythagoras(3,4,5)
 
-student.fullName = student.firstName + ' ' + student.lastName;
-console.log (student.fullName)
-
-student.fullName = 'nina tsiklauri';
-console.log(student.fullName);
-
-var result = student.fullName + ' ' + 'is' + ' ' + student.age + ' ' + 'years old' + ' ' + 'and her roommate is' + ' ' + student.roommate.fullName;
 console.log(result);
 
-//task 4 
+//task 3 
 
-let fruitsArr = ['banana', 'orange', 'apple', 'mango', 2, 12 ];
-let i = 0;
+let minMaxArray = [2,14,25,75,11,6]
+function minMax (array){
+    let max = array [0];
+    let min = array [0];
+    for(i = 0; i <array.length; i++){
+        if (array[i] > max){
+            max = array[i];
+        }
+    }
+    for(i = 0; i <array.length; i++){
+        if(array[i] < min){
+            min = array[i];
+        }
+    }
+    return (`min value is ${min} and max value is ${max}`);
 
-while(typeof fruitsArr[i] == 'string') {
-    console.log(fruitsArr[i]);
-    i++;
+}
+let result2 = minMax(minMaxArray);
+console.log(result2);
+
+//task 4
+
+function angleCounter (angles) 
+{
+    
+    
+    switch (true) 
+    {
+     
+       case (angles >= 0 && angles < 90) : 
+            console.log(`Acute angle: An angle between 0 and 90 degrees.`);
+            break ;
+
+       case (angles == 90) : 
+            console.log(`Right angle: An 90 degree angle.`);
+            break ;
+         
+       case (angles > 90 && angles < 180 ) :
+            console.log(`Obtuse angle: An angle between 90 and 180 degrees.`);
+            break ;
+
+       case (angles == 180) :
+            console.log(`Straight angle: A 180 degree angle.`);
+            break ;
+
+        default :
+            console.log(`More Than 180 `);
+            break;
+    }
 }
 
-// task5
+angleCounter (183);
 
-let numArr = [12, 23,43,11, 9,2,121,90];
 
-for( let i = 0; i < numArr.length;i++){
-    let element = numArr[i];
-    if(element > 31 && element%2 == 0){
-        console.log('metia luwia');
-    }
-    else if (element < 31 && element%2 == 1){
-        console.log('naklebia 31 da kenti')
-    }
-}
+//task 5
+
+
